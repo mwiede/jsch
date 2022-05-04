9b481d91defb1f1551c16ca52fa1c51bfdbcbfb7
k2 <zhangzexin@topstack.com>
2022-05-04 15:13:31 +0800

    fix StandardCharsets NoClassDefFoundError (Added in API level 19 StandardCharsets)

--- src/main/java/com/jcraft/jsch/CharsetUtil.java
+++ src/main/java/com/jcraft/jsch/CharsetUtil.java
@@ -3,5 +3,5 @@ package com.jcraft.jsch;
 import java.nio.charset.Charset;
 
 public class CharsetUtil {
-    public static Charset UTF_8 = sun.nio.cs.UTF_8.INSTANCE;
+    public static Charset UTF_8 = Charset.forName("UTF-8");
 }
