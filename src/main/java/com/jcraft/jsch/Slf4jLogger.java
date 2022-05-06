7ef222a9bd00d985205198596ee37f4e53d063b2
kimmerin <github@kimmeringer.de>
2022-05-06 14:53:52 +0200

    fixed formatting

--- src/main/java/com/jcraft/jsch/Slf4jLogger.java
+++ src/main/java/com/jcraft/jsch/Slf4jLogger.java
@@ -3,11 +3,17 @@ package com.jcraft.jsch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * JSch logger to log entries using the SLF4J framework
+ */
 public class Slf4jLogger implements com.jcraft.jsch.Logger {
 
   private static final Logger stlogger = LoggerFactory.getLogger(JSch.class);
   private final Logger logger;
 
+  /**
+   * Creates a new instance of Slf4jLogger
+   */
   public Slf4jLogger() {
     this(stlogger);
   }
@@ -18,16 +24,16 @@ public class Slf4jLogger implements com.jcraft.jsch.Logger {
 
   @Override
   public boolean isEnabled(int level) {
-    switch(level) {
-        case com.jcraft.jsch.Logger.DEBUG:
-            return logger.isDebugEnabled();
-        case com.jcraft.jsch.Logger.ERROR:
-        case com.jcraft.jsch.Logger.FATAL:
-            return logger.isErrorEnabled();
-        case com.jcraft.jsch.Logger.INFO:
-            return logger.isInfoEnabled();
-        case com.jcraft.jsch.Logger.WARN:
-            return logger.isWarnEnabled();
+    switch (level) {
+      case com.jcraft.jsch.Logger.DEBUG:
+        return logger.isDebugEnabled();
+      case com.jcraft.jsch.Logger.ERROR:
+      case com.jcraft.jsch.Logger.FATAL:
+        return logger.isErrorEnabled();
+      case com.jcraft.jsch.Logger.INFO:
+        return logger.isInfoEnabled();
+      case com.jcraft.jsch.Logger.WARN:
+        return logger.isWarnEnabled();
     }
     return logger.isTraceEnabled();
   }
