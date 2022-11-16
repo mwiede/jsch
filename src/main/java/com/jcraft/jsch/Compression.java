a4709e09e12ccaa8858c09d182118e9688175db8
Jeremy Norris <jeremy.norris@localbackhaul.com>
2022-11-16 09:40:26 -0600

    Explictly free resources in Compression implementations.

--- src/main/java/com/jcraft/jsch/Compression.java
+++ src/main/java/com/jcraft/jsch/Compression.java
@@ -37,6 +37,8 @@ public interface Compression{
     init(type, level);
   }
 
+  default void end() {}
+
   void init(int type, int level);
   byte[] compress(byte[] buf, int start, int[] len);
   byte[] uncompress(byte[] buf, int start, int[] len);
