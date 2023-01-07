c9f5a9f1bbdad334666cbd9f613daf36d6dfd475
Jeremy Norris <jeremy.norris@lbnetworks.co>
2023-01-07 10:23:55 -0600

    Add unit tests for Log4j2Logger.

---
+++ src/test/java/com/jcraft/jsch/Log4j2LoggerTest.java
@@ -0,0 +1,160 @@
+package com.jcraft.jsch;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.junit.LoggerContextSource;
+import org.apache.logging.log4j.junit.Named;
+import org.apache.logging.log4j.message.Message;
+import org.apache.logging.log4j.test.appender.ListAppender;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@LoggerContextSource("Log4j2LoggerTest.xml")
+public class Log4j2LoggerTest {
+  private Exception testException = new Exception("dummy exception");
+  private ListAppender appender;
+
+  public Log4j2LoggerTest(@Named("List") ListAppender appender) {
+    this.appender = appender;
+  }
+
+  @BeforeEach
+  void resetLogger() {
+    appender.clear();
+  }
+
+  @Test
+  void testIsEnabled() {
+    Log4j2Logger ll = new Log4j2Logger();
+
+    setLevel(Level.ALL);
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should be enabled");
+    assertTrue(ll.isEnabled(-1), "trace should be enabled");
+
+    setLevel(Level.DEBUG);
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should be enabled");
+    assertFalse(ll.isEnabled(-1), "trace should not be enabled");
+
+    setLevel(Level.ERROR);
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should not be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should not be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should not be enabled");
+    assertFalse(ll.isEnabled(-1), "trace should not be enabled");
+
+    setLevel(Level.FATAL);
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should not be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should not be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should not be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should not be enabled");
+    assertFalse(ll.isEnabled(-1), "trace should not be enabled");
+
+    setLevel(Level.INFO);
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should not be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should be enabled");
+    assertFalse(ll.isEnabled(-1), "trace should not be enabled");
+
+    setLevel(Level.OFF);
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should not be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should not be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should not be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should not be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should not be enabled");
+    assertFalse(ll.isEnabled(-1), "trace should not be enabled");
+
+    setLevel(Level.TRACE);
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should be enabled");
+    assertTrue(ll.isEnabled(-1), "trace should be enabled");
+
+    setLevel(Level.WARN);
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.DEBUG), "debug should not be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.ERROR), "error should be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.FATAL), "fatal should be enabled");
+    assertFalse(ll.isEnabled(com.jcraft.jsch.Logger.INFO), "info should not be enabled");
+    assertTrue(ll.isEnabled(com.jcraft.jsch.Logger.WARN), "warn should be enabled");
+    assertFalse(ll.isEnabled(-1), "trace should not be enabled");
+  }
+
+  @Test
+  void testLogging() {
+    Log4j2Logger ll = new Log4j2Logger();
+
+    List<String> expectedMessages =
+        Arrays.asList("debug message", "debug message with null cause", "debug message with cause");
+    List<Optional<Throwable>> expectedExceptions =
+        Arrays.asList(Optional.empty(), Optional.ofNullable(null), Optional.of(testException));
+
+    setLevel(Level.TRACE);
+    ll.log(-1, "debug message");
+    ll.log(-1, "debug message with null cause", null);
+    ll.log(-1, "debug message with cause", testException);
+    checkMessages(expectedMessages, expectedExceptions);
+
+    setLevel(Level.TRACE);
+    ll.log(com.jcraft.jsch.Logger.FATAL, "debug message");
+    ll.log(com.jcraft.jsch.Logger.FATAL, "debug message with null cause", null);
+    ll.log(com.jcraft.jsch.Logger.FATAL, "debug message with cause", testException);
+    checkMessages(expectedMessages, expectedExceptions);
+
+    setLevel(Level.ERROR);
+    ll.log(-1, "debug message");
+    ll.log(-1, "debug message with null cause", null);
+    ll.log(-1, "debug message with cause", testException);
+    checkMessages(Collections.emptyList(), Collections.emptyList());
+
+    setLevel(Level.ERROR);
+    ll.log(com.jcraft.jsch.Logger.FATAL, "debug message");
+    ll.log(com.jcraft.jsch.Logger.FATAL, "debug message with null cause", null);
+    ll.log(com.jcraft.jsch.Logger.FATAL, "debug message with cause", testException);
+    checkMessages(expectedMessages, expectedExceptions);
+  }
+
+  private void checkMessages(
+      List<String> expectedMessages, List<Optional<Throwable>> expectedExceptions) {
+    List<LogEvent> events = appender.getEvents();
+    appender.clear();
+    List<String> actualMessages =
+        events.stream()
+            .map(LogEvent::getMessage)
+            .map(Message::getFormattedMessage)
+            .collect(Collectors.toList());
+    List<Optional<Throwable>> actualExceptions =
+        events.stream()
+            .map(LogEvent::getThrown)
+            .map(Optional::ofNullable)
+            .collect(Collectors.toList());
+    assertEquals(expectedMessages, actualMessages, "mismatch in logged messages");
+    assertEquals(expectedExceptions, actualExceptions, "mismatch in logged exceptions");
+  }
+
+  private static void setLevel(Level level) {
+    Configurator.setLevel(JSch.class, level);
+  }
+}
