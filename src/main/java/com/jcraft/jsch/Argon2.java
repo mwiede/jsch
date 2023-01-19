ff55eb9639942a46fa103c4819c516c84e8412ab
Jeremy Norris <jeremy.norris@lbnetworks.co>
2023-01-19 14:37:04 -0600

    Mark Argon2 type and versions as final.

--- src/main/java/com/jcraft/jsch/Argon2.java
+++ src/main/java/com/jcraft/jsch/Argon2.java
@@ -30,11 +30,11 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package com.jcraft.jsch;
 
 public interface Argon2 {
-  public static int ARGON2D = 0;
-  public static int ARGON2I = 1;
-  public static int ARGON2ID = 2;
-  public static int V10 = 0x10;
-  public static int V13 = 0x13;
+  public static final int ARGON2D = 0;
+  public static final int ARGON2I = 1;
+  public static final int ARGON2ID = 2;
+  public static final int V10 = 0x10;
+  public static final int V13 = 0x13;
 
   void init(byte[] salt, int iteration, int type, byte[] additional, byte[] secret, int memory, int parallelism, int version) throws Exception;
   byte[] getKey(byte[] pass, int size);
