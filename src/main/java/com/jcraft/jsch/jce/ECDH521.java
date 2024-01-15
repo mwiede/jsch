449bb0a75f586068d44d125947d454d1905f0f6c
Jeremy Norris <jeremy.norris@lbnetworks.co>
2024-01-15 13:31:38 -0600

    Remove unneeded implements statements.

--- src/main/java/com/jcraft/jsch/jce/ECDH521.java
+++ src/main/java/com/jcraft/jsch/jce/ECDH521.java
@@ -26,7 +26,7 @@
 
 package com.jcraft.jsch.jce;
 
-public class ECDH521 extends ECDHN implements com.jcraft.jsch.ECDH {
+public class ECDH521 extends ECDHN {
   public void init() throws Exception {
     super.init(521);
   }
