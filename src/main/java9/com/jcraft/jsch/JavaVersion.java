b3520090e00d7dae7fe7af5a3604cbfc2dd12dc9
Matthias Wiedemann <mwiede@gmx.de>
2024-02-02 10:00:25 +0000

    suppress deprecation error from forbiddenapis check

--- src/main/java9/com/jcraft/jsch/JavaVersion.java
+++ src/main/java9/com/jcraft/jsch/JavaVersion.java
@@ -1,7 +1,10 @@
 package com.jcraft.jsch;
 
+import com.jcraft.jsch.annotations.SuppressForbiddenApi;
+
 final class JavaVersion {
 
+  @SuppressForbiddenApi("jdk-deprecated")
   static int getVersion() {
     return Runtime.version().major();
   }
