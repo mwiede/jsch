5a48ba22b2d882ac32806d8b1315c57bca50722d
asingh1508 <53421552+asingh1508@users.noreply.github.com>
2025-03-07 11:34:11 +0530

    Update Channel.java

--- src/main/java/com/jcraft/jsch/Channel.java
+++ src/main/java/com/jcraft/jsch/Channel.java
@@ -32,7 +32,6 @@ import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.Vector;
-import java.util.I
   
 public abstract class Channel {
 
