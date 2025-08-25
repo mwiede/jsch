29e7af7ee3bf1504becb5792ad82177aab258bd6
Jeremy Norris <jeremy.norris@lbnetworks.co>
2025-08-25 13:52:38 -0500

    Refine previous changes based upon suggestions.

--- src/main/java/com/jcraft/jsch/Channel.java
+++ src/main/java/com/jcraft/jsch/Channel.java
@@ -586,39 +586,37 @@ public abstract class Channel {
     return close;
   }
 
-  void _disconnect() {
+  public void disconnect() {
     // System.err.println(this+":disconnect "+io+" "+connected);
     // Thread.dumpStack();
 
-    synchronized (this) {
-      if (!connected) {
-        return;
-      }
-      connected = false;
-    }
-
-    close();
-
-    eof_remote = eof_local = true;
-
-    thread = null;
-
     try {
-      if (io != null) {
-        io.close();
-      }
-    } catch (Exception e) {
-      // e.printStackTrace();
-    }
-    // io=null;
-  }
 
-  public void disconnect() {
-    try {
-      _disconnect();
+      synchronized (this) {
+        if (!connected) {
+          return;
+        }
+        connected = false;
+      }
+
+      close();
+
+      eof_remote = eof_local = true;
+
+      thread = null;
+
+      try {
+        if (io != null) {
+          io.close();
+        }
+      } catch (Exception e) {
+        // e.printStackTrace();
+      }
+      // io=null;
     } finally {
-      if (session != null) {
-        session.delChannel(this);
+      Session _session = this.session;
+      if (_session != null) {
+        _session.delChannel(this);
       }
     }
   }
