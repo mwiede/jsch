17d933b141b42fcd01d5d01995bb10981b52f0a7
Luigi De Masi <ldemasi@redhat.com>
2025-09-18 16:19:18 +0200

    Add support for OpenSSH certificates, resolve #31 - Fixes after code review

--- src/main/java/com/jcraft/jsch/Buffer.java
+++ src/main/java/com/jcraft/jsch/Buffer.java
@@ -28,17 +28,17 @@ package com.jcraft.jsch;
 
 public class Buffer {
   final byte[] tmp = new byte[4];
-  protected byte[] buffer;
+  byte[] buffer;
 
   // write position
   // Tracks the current writing position in the buffer and points to where the next
   // write operation will occur and increments as data is written
-  protected int index;
+  int index;
 
   // read position - Tracks the current reading position in the buffer and points to where the next
   // read operation will
   // occur and increments as data is read
-  protected int s;
+  int s;
 
   public Buffer(int size) {
     buffer = new byte[size];
