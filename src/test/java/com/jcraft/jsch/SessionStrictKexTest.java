c94787e812f2170d2b77fd837cba24b2c64a8ddc
code-docker <vinay.jagannath@outlook.com>
2025-10-21 10:18:45 +0530

    Adhere to https://google.github.io/styleguide/javaguide.html\#s3.3.3-import-ordering-and-spacing

--- src/test/java/com/jcraft/jsch/SessionStrictKexTest.java
+++ src/test/java/com/jcraft/jsch/SessionStrictKexTest.java
@@ -1,10 +1,10 @@
 package com.jcraft.jsch;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests for the private Session.checkServerStrictKex() method via reflection. We craft the internal
