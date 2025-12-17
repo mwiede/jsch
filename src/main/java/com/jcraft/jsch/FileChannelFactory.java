b942fa6c89796c95bcff7a206855e8e6af5cd04c
bananensplit <jeremiasz.zrolka@gmail.com>
2025-12-17 11:01:52 +0100

    Run formatter for new files

--- src/main/java/com/jcraft/jsch/FileChannelFactory.java
+++ src/main/java/com/jcraft/jsch/FileChannelFactory.java
@@ -4,5 +4,5 @@ import java.io.IOException;
 import java.nio.channels.FileChannel;
 
 public interface FileChannelFactory {
-	FileChannel open(String path) throws IOException;
+  FileChannel open(String path) throws IOException;
 }
